d8ade7b4320a1a14d38f79a0893bc85b22e6bb47
sarawut852 <sarawutx233@gmail.com>
2025-07-17 11:42:05 +0700

    week-3 commit

---
+++ wee-3/ex1.py
@@ -0,0 +1,10 @@
+decimal = input("input decimal here : ")
+decimal = int(decimal)
+
+binaly = bin(decimal)
+hex = hex(decimal)
+oct = oct(decimal)
+
+print("this is binary",binaly)
+print("this is hex",hex)
+print("this is oct",oct)
